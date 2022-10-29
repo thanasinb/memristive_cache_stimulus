0bd196c0852f3482c024ca6b1d5a7ce05e407bc7
Thanasin Bunnam <55657279+thanasinb@users.noreply.github.com>
2022-10-29 06:04:55 +0000

    active low

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ for x in range(1024):
   if x == select:
     f.write('V_wl_sel  (WordLine_bar<' + str(x) + '> gnd!) vsource dc=0\n')
   else:
-    f.write('V_wl_' + str(x) + '  (WordLine_bar<' + str(x) + '> gnd!) vsource dc=0\n')
+    f.write('V_wl_' + str(x) + '  (WordLine_bar<' + str(x) + '> gnd!) vsource dc=1.2\n')
 f.close()
 
-# f = open("stimulus_data.txt", "w")
-# for x in range(32):
-#   f.write('V_data_' + str(x) + ' (Data<' + str(x) + '> gnd!) vsource dc=0\n')
-# f.close()
+f = open("stimulus_data.txt", "w")
+for x in range(32):
+  f.write('V_data_' + str(x) + ' (Data<' + str(x) + '> gnd!) vsource dc=1.2\n')
+f.close()
 
 print('Done!')
