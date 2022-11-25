46d455b206ec6dc31b5b90134742c9407501c549
thanasin <thanasin.b@gmail.com>
2022-11-25 12:05:28 +0700

    select word#1000, data#FF00FF00

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 f = open("stimulus_wl.txt", "w")
 select = 1000
+data = [
+  0, 0, 0, 0, 0, 0, 0, 0,
+  1, 1, 1, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  1, 1, 1, 1, 1, 1, 1, 1,
+]
+
 for x in range(1024):
   if x == select:
     f.write('V_wl_sel  (WordLine_bar<' + str(x) + '> gnd!) vsource dc=0\n')
@@ -8,8 +15,13 @@ for x in range(1024):
 f.close()
 
 f = open("stimulus_data.txt", "w")
-for x in range(32):
-  f.write('V_data_' + str(x) + ' (Data<' + str(x) + '> gnd!) vsource dc=1.2\n')
+i = 0
+for x in data:
+  if x == 0:
+    f.write('V_data_' + str(i) + ' (Data<' + str(i) + '> gnd!) vsource dc=0\n')
+  if x == 1:
+    f.write('V_data_' + str(i) + ' (Data<' + str(i) + '> gnd!) vsource dc=1.2\n')
+  i = i + 1
 f.close()
 
 print('Done!')
