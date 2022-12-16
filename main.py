318b1284dd3636c1b58940cc73c7a9414140984e
thanasin <thanasin.b@gmail.com>
2022-12-16 17:23:24 +0700

    1024 words

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ data = [
 # 1, 1, 1, 1, 1, 1, 1, 1,
 # 0, 0, 0, 0, 0, 0, 0, 0,
 
-select_word = 0
-total_word = 2
+select_word_1 = 0
+select_word_2 = 1
+total_word = 1024
 read = True
 
 f = open("stimulus_base.scs", "w")
@@ -23,20 +24,25 @@ f.close()
 
 f = open("stimulus_wl.scs", "w")
 for x in range(total_word):
-  if x == select_word:
-    # f.write('V_wl' + str(x) + '  (WordLine_bar<' + str(x) + '> gnd!) vsource dc=0\n')
+  if x == select_word_1:
     f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    0\n')
     f.write('+ 500p 0\n')
     f.write('+ 510p 1.2\n')
     f.write('+ \\]\n')
-  else:
-    # f.write('V_wl_' + str(x) + '  (WordLine_bar<' + str(x) + '> gnd!) vsource dc=1.2\n')
+  elif x == select_word_2:
     f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    1.2\n')
     f.write('+ 500p 1.2\n')
     f.write('+ 510p 0\n')
     f.write('+ \\]\n')
+  else:
+    f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
+    f.write('+ 0    1.2\n')
+    f.write('+ 500p 1.2\n')
+    f.write('+ 510p 1.2\n')
+    f.write('+ \\]\n')
+
 f.close()
 
 f = open("stimulus_data.scs", "w")
