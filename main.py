cc23e268ea4d6f4684061e52a36c50b0d507a20f
thanasin <thanasin.b@gmail.com>
2023-01-06 12:53:27 +0700

    Write and Read 2 Words

--- main.py
+++ main.py
@@ -16,10 +16,24 @@ read = True
 f = open("stimulus_base.scs", "w")
 f.write("Vdd    (vdd! gnd!)   vsource dc=1.2\n")
 f.write("V_Read (V_Read gnd!) vsource dc=1.2\n")
-if(read):
-  f.write("V_RW   (RW gnd!)     vsource dc=1.2\n")
-else:
-  f.write("V_RW   (RW gnd!)     vsource dc=0\n")
+# if(read):
+#   f.write("V_RW   (RW gnd!)     vsource dc=1.2\n")
+# else:
+#   f.write("V_RW   (RW gnd!)     vsource dc=0\n")
+
+f.write('V_RW   (RW gnd!)     vsource type=pwl wave=\\[\n')
+f.write('+ 0    0\n')
+f.write('+ 500p 0\n')
+f.write('+ 510p 1.2\n')
+f.write('+ 1000p 1.2\n')
+f.write('+ 1010p 0\n')
+f.write('+ 1500p 0\n')
+f.write('+ 1510p 1.2\n')
+f.write('+ 2000p 1.2\n')
+f.write('+ 2010p 0\n')
+f.write('+ 2500p 0\n')
+f.write('+ \\]\n')
+
 f.close()
 
 f = open("stimulus_wl.scs", "w")
@@ -27,20 +41,20 @@ for x in range(total_word):
   if x == select_word_1:
     f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    0\n')
-    f.write('+ 500p 0\n')
-    f.write('+ 510p 1.2\n')
+    f.write('+ 1000p 0\n')
+    f.write('+ 1010p 1.2\n')
     f.write('+ \\]\n')
   elif x == select_word_2:
     f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    1.2\n')
-    f.write('+ 500p 1.2\n')
-    f.write('+ 510p 0\n')
+    f.write('+ 1000p 1.2\n')
+    f.write('+ 1010p 0\n')
     f.write('+ \\]\n')
   else:
     f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    1.2\n')
-    f.write('+ 500p 1.2\n')
-    f.write('+ 510p 1.2\n')
+    f.write('+ 1000p 1.2\n')
+    f.write('+ 1010p 1.2\n')
     f.write('+ \\]\n')
 
 f.close()
@@ -49,9 +63,19 @@ f = open("stimulus_data.scs", "w")
 i = 0
 for x in data:
   if x == 0:
-    f.write('V_data_' + str(i) + ' (Data\\\\<' + str(i) + '\\\\> gnd!) vsource dc=0\n')
+    # f.write('V_data_' + str(i) + ' (Data\\\\<' + str(i) + '\\\\> gnd!) vsource dc=0\n')
+    f.write('V_data_' + str(i) + ' (Data\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
+    f.write('+ 0    0\n')
+    f.write('+ 1000p 0\n')
+    f.write('+ 1010p 1.2\n')
+    f.write('+ \\]\n')
   if x == 1:
-    f.write('V_data_' + str(i) + ' (Data\\\\<' + str(i) + '\\\\> gnd!) vsource dc=1.2\n')
+    # f.write('V_data_' + str(i) + ' (Data\\\\<' + str(i) + '\\\\> gnd!) vsource dc=1.2\n')
+    f.write('V_data_' + str(i) + ' (Data\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
+    f.write('+ 0    1.2\n')
+    f.write('+ 1000p 1.2\n')
+    f.write('+ 1010p 0\n')
+    f.write('+ \\]\n')
   i = i + 1
 f.close()
 
