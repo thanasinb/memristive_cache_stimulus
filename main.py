f8a6163b8b91be1c7df3c4de9f9e235409ac5304
thanasin <thanasin.b@gmail.com>
2023-01-06 14:45:27 +0700

    200ps step

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ data = [
 
 select_word_1 = 0
 select_word_2 = 1
-total_word = 1024
+total_word = 2
 read = True
 
 f = open("stimulus_base.scs", "w")
@@ -22,17 +22,18 @@ f.write("V_Read (V_Read gnd!) vsource dc=1.2\n")
 #   f.write("V_RW   (RW gnd!)     vsource dc=0\n")
 
 f.write('V_RW   (RW gnd!)     vsource type=pwl wave=\\[\n')
-f.write('+ 0    0\n')
-f.write('+ 200p 0\n')
-f.write('+ 210p 1.2\n')
-f.write('+ 400p 1.2\n')
-f.write('+ 410p 0\n')
-f.write('+ 600p 0\n')
-f.write('+ 610p 1.2\n')
-f.write('+ 800p 1.2\n')
-f.write('+ 810p 0\n')
-f.write('+ 1000p 0\n')
-f.write('+ \\]\n')
+for x in range(total_word):
+  f.write('+ 0    0\n')
+  f.write('+ 200p 0\n')
+  f.write('+ 210p 1.2\n')
+  f.write('+ 400p 1.2\n')
+  f.write('+ 410p 0\n')
+  f.write('+ 600p 0\n')
+  f.write('+ 610p 1.2\n')
+  f.write('+ 800p 1.2\n')
+  f.write('+ 810p 0\n')
+  f.write('+ 1000p 0\n')
+  f.write('+ \\]\n')
 
 f.close()
 
@@ -47,8 +48,8 @@ for x in range(total_word):
   elif x == select_word_2:
     f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    1.2\n')
-    f.write('+ 800p 1.2\n')
-    f.write('+ 810p 0\n')
+    f.write('+ 400p 1.2\n')
+    f.write('+ 410p 0\n')
     f.write('+ \\]\n')
   else:
     f.write('V_wl_' + str(x) + ' (WordLine_bar\\\\<' + str(x) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
