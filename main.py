f1ba9d6ce2ee6740279de4fbca63a9323d730de2
thanasinb <thanasin.b@gmail.com>
2023-03-14 10:14:42 +0700

    Write 0xFFFFFFFF WL 8-11

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ select_word_2 = 1
 num_word = 1024
 read = True
 data_mode_switching = False
-start_WL = 4
+start_WL = 8
 
 f = open("stimulus_base.scs", "w")
 f.write("Vdd    (vdd! gnd!)   vsource dc=1.2\n")
