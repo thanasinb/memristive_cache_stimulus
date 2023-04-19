2e01f1b845387b5bb3e706c1be8a04471c29739c
thanasin <thanasin.b@gmail.com>
2023-04-19 22:16:54 +0700

    Shift data 10ps before

--- main.py
+++ main.py
@@ -89,10 +89,10 @@ for x in data:
     f.write('+ 0    0\n')
     if (data_mode_switching):
       for x in range(int(num_word/2)):
-        f.write('+ ' + str(step_1-1) + '70p 0\n')
-        f.write('+ ' + str(step_1-1) + '90p 1.2\n')
-        f.write('+ ' + str(step_2-1) + '70p 1.2\n')
-        f.write('+ ' + str(step_2-1) + '90p 0\n')
+        f.write('+ ' + str(step_1-1) + '60p 0\n')
+        f.write('+ ' + str(step_1-1) + '80p 1.2\n')
+        f.write('+ ' + str(step_2-1) + '60p 1.2\n')
+        f.write('+ ' + str(step_2-1) + '80p 0\n')
         step_1 = step_1 + 10
         step_2 = step_2 + 10
     f.write('+ \\]\n')
@@ -103,10 +103,10 @@ for x in data:
     f.write('+ 0    1.2\n')
     if (data_mode_switching):
       for x in range(int(num_word/2)):
-        f.write('+ ' + str(step_1-1) + '70p 1.2\n')
-        f.write('+ ' + str(step_1-1) + '90p 0\n')
-        f.write('+ ' + str(step_2-1) + '70p 0\n')
-        f.write('+ ' + str(step_2-1) + '90p 1.2\n')
+        f.write('+ ' + str(step_1-1) + '60p 1.2\n')
+        f.write('+ ' + str(step_1-1) + '80p 0\n')
+        f.write('+ ' + str(step_2-1) + '60p 0\n')
+        f.write('+ ' + str(step_2-1) + '80p 1.2\n')
         step_1 = step_1 + 10
         step_2 = step_2 + 10
     f.write('+ \\]\n')
@@ -122,10 +122,10 @@ for x in tag_data:
     f.write('V_tag_data_' + str(i) + ' (Tag_Data\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    0\n')
     for x in range(int(num_word/2)):
-      f.write('+ ' + str(step_1-1) + '70p 0\n')
-      f.write('+ ' + str(step_1-1) + '90p 1.2\n')
-      f.write('+ ' + str(step_2-1) + '70p 1.2\n')
-      f.write('+ ' + str(step_2-1) + '90p 0\n')
+      f.write('+ ' + str(step_1-1) + '60p 0\n')
+      f.write('+ ' + str(step_1-1) + '80p 1.2\n')
+      f.write('+ ' + str(step_2-1) + '60p 1.2\n')
+      f.write('+ ' + str(step_2-1) + '80p 0\n')
       step_1 = step_1 + 10
       step_2 = step_2 + 10
     f.write('+ \\]\n')
@@ -135,10 +135,10 @@ for x in tag_data:
     f.write('V_tag_data_' + str(i) + ' (Tag_Data\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
     f.write('+ 0    1.2\n')
     for x in range(int(num_word/2)):
-      f.write('+ ' + str(step_1-1) + '70p 1.2\n')
-      f.write('+ ' + str(step_1-1) + '90p 0\n')
-      f.write('+ ' + str(step_2-1) + '70p 0\n')
-      f.write('+ ' + str(step_2-1) + '90p 1.2\n')
+      f.write('+ ' + str(step_1-1) + '60p 1.2\n')
+      f.write('+ ' + str(step_1-1) + '80p 0\n')
+      f.write('+ ' + str(step_2-1) + '60p 0\n')
+      f.write('+ ' + str(step_2-1) + '80p 1.2\n')
       step_1 = step_1 + 10
       step_2 = step_2 + 10
     f.write('+ \\]\n')
