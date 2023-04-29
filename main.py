37f24ec58d29bbd307e6144de1085c371f568436
thanasin <thanasin.b@gmail.com>
2023-04-29 23:51:18 +0700

    include bar signals

--- main.py
+++ main.py
@@ -40,6 +40,17 @@ for x in range(num_word):
   step_2 = step_2 + 5
 f.write('+ \\]\n')
 
+f.write('V_RW_bar   (RW_bar gnd!)     vsource type=pwl wave=\\[\n')
+f.write('+ 0    1.2\n')
+for x in range(num_word):
+  f.write('+ ' + str(step_1-1) + '90p 1.2\n')
+  f.write('+ ' + str(step_1) + '00p 0\n')
+  f.write('+ ' + str(step_2-1) + '90p 0\n')
+  f.write('+ ' + str(step_2) + '00p 1.2\n')
+  step_1 = step_1 + 5
+  step_2 = step_2 + 5
+f.write('+ \\]\n')
+
 step_1 = 7
 step_2 = 10
 f.write('V_clk   (clk gnd!)     vsource type=pwl wave=\\[\n')
@@ -96,6 +107,17 @@ for x in data:
         step_1 = step_1 + 10
         step_2 = step_2 + 10
     f.write('+ \\]\n')
+    f.write('V_data_bar_' + str(i) + ' (Data_bar\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
+    f.write('+ 0    1.2\n')
+    if (data_mode_switching):
+      for x in range(int(num_word/2)):
+        f.write('+ ' + str(step_1-1) + '90p 1.2\n')
+        f.write('+ ' + str(step_1) + '00p 0\n')
+        f.write('+ ' + str(step_2-1) + '90p 0\n')
+        f.write('+ ' + str(step_2) + '00p 1.2\n')
+        step_1 = step_1 + 10
+        step_2 = step_2 + 10
+    f.write('+ \\]\n')
   if x == 1:
     step_1 = 5
     step_2 = 10
@@ -107,6 +129,17 @@ for x in data:
         f.write('+ ' + str(step_1) + '00p 0\n')
         f.write('+ ' + str(step_2-1) + '90p 0\n')
         f.write('+ ' + str(step_2) + '00p 1.2\n')
+        step_1 = step_1 + 10
+        step_2 = step_2 + 10
+    f.write('+ \\]\n')
+    f.write('V_data_bar_' + str(i) + ' (Data_bar\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
+    f.write('+ 0    0\n')
+    if (data_mode_switching):
+      for x in range(int(num_word/2)):
+        f.write('+ ' + str(step_1-1) + '90p 0\n')
+        f.write('+ ' + str(step_1) + '00p 1.2\n')
+        f.write('+ ' + str(step_2-1) + '90p 1.2\n')
+        f.write('+ ' + str(step_2) + '00p 0\n')
         step_1 = step_1 + 10
         step_2 = step_2 + 10
     f.write('+ \\]\n')
@@ -129,6 +162,16 @@ for x in tag_data:
       step_1 = step_1 + 10
       step_2 = step_2 + 10
     f.write('+ \\]\n')
+    f.write('V_tag_data_bar_' + str(i) + ' (Tag_Data_bar\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
+    f.write('+ 0    1.2\n')
+    for x in range(int(num_word/2)):
+      f.write('+ ' + str(step_1-1) + '90p 1.2\n')
+      f.write('+ ' + str(step_1) + '00p 0\n')
+      f.write('+ ' + str(step_2-1) + '90p 0\n')
+      f.write('+ ' + str(step_2) + '00p 1.2\n')
+      step_1 = step_1 + 10
+      step_2 = step_2 + 10
+    f.write('+ \\]\n')
   if x == 1:
     step_1 = 5
     step_2 = 10
@@ -139,6 +182,16 @@ for x in tag_data:
       f.write('+ ' + str(step_1) + '00p 0\n')
       f.write('+ ' + str(step_2-1) + '90p 0\n')
       f.write('+ ' + str(step_2) + '00p 1.2\n')
+      step_1 = step_1 + 10
+      step_2 = step_2 + 10
+    f.write('+ \\]\n')
+    f.write('V_tag_data_bar_' + str(i) + ' (Tag_Data_bar\\\\<' + str(i) + '\\\\> gnd!) vsource type=pwl wave=\\[\n')
+    f.write('+ 0    0\n')
+    for x in range(int(num_word/2)):
+      f.write('+ ' + str(step_1-1) + '90p 0\n')
+      f.write('+ ' + str(step_1) + '00p 1.2\n')
+      f.write('+ ' + str(step_2-1) + '90p 1.2\n')
+      f.write('+ ' + str(step_2) + '00p 0\n')
       step_1 = step_1 + 10
       step_2 = step_2 + 10
     f.write('+ \\]\n')
